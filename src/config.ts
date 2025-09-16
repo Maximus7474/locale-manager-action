0d1c6a38467d9896aabe21b9dd563484dd2c195e
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-16 17:27:09 +0200

    feat(src): config file - sanitizes inputs and required config fields

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+interface Config {
+  localeRepo: string;
+  localeRepoToken: string | null;
+  resourceName: string;
+  localeDirectory: string;
+}
+
+const config: Config = {
+  localeRepo: core.getInput('locale-repo', { required: true }),
+  localeRepoToken: core.getInput('locale-repo-token', { required: false }),
+
+  resourceName: core.getInput('resource-name', { required: false }),
+  localeDirectory: core.getInput('locale-directory', { required: false }),
+};
+
+if (!config.localeRepo) {
+  throw new Error("The 'locale-repo' input is required.");
+}
+
+if (!config.resourceName) {
+  const { owner, repo } = github.context.repo;
+  const currentRepository = `${owner}/${repo}`;
+
+  core.warning(`The 'resource-name' was not specified, using '${currentRepository}'. Please define explicitly the repositor as 'owner/repository'.`);
+  
+  config.resourceName = currentRepository;
+}
+
+if (!config.localeDirectory) {
+  core.warning("The 'locale-directory' was not specified, using './locales/'. Please define explicitly the path.");
+}
+
+export default config;
